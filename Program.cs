d318355ec26015a3f8eb97d21d6be9e27db65cda
suxi <suxi@xiaohulu.com>
2018-10-16 19:14:20 +0800

    minor bug fix

--- Program.cs
+++ Program.cs
@@ -11,46 +11,41 @@ namespace Duple
         static void Main(string[] args)
         {
             var files = new List<string>();
-            if (args.Length > 0)
+            foreach (var item in args)
             {
-                foreach (var item in args)
+                var root = new DirectoryInfo(item);
+                if (root.Exists)
                 {
-                    var root = new DirectoryInfo(item);
-                    if (root.Exists)
-                    {
-                        var fileNames = from dir in Directory.EnumerateFiles(root.FullName,"*.*", SearchOption.AllDirectories)
-                                        select dir;
-                        files.AddRange(fileNames);
-                    }
-                }
-                if (files.Count == 0)
-                {
-                    Console.WriteLine("Duple [path1] [path2] ...");
-                    return;
+                    var fileNames = from dir in Directory.EnumerateFiles(root.FullName,"*.*", SearchOption.AllDirectories)
+                                    select dir;
+                    files.AddRange(fileNames);
                 }
             }
-
-
+            if (files.Count == 0)
+            {
+                Console.WriteLine("Duple [path1] [path2] ...");
+                return;
+            }
             
             var videos = from file in files.AsParallel()
                             let ext = Path.GetExtension(file)
                             where ext.ToLower() == ".mp4" || ext.ToLower() == ".mkv" || ext.ToLower() == ".avi"
-                            let dir = Path.GetDirectoryName(file)
+                            let date = File.GetLastWriteTime(file)
                             let name = Path.GetFileNameWithoutExtension(file)
                             let path = Path.GetFullPath(file)
                             let size = new FileInfo(file).Length
-                            select new {name,dir,path,size};
+                            select new {name,date,path,size};
             var reg = new Regex(@"[a-zA-Z]{2,5}-?\d{0,3}");
             var last = "";
             foreach (var item in videos.OrderBy(item => reg.Match(item.name).Value))
             {
                 if (last == reg.Match(item.name).Value.Replace("-","").ToUpper() && reg.Match(item.name).Value.Replace("-","").Length > 0)
                 {
-                    Console.WriteLine($"*{item.name} \t{item.path}\t{item.size}");
+                    Console.WriteLine($"*{item.name} \t{item.date:yyyy/MM/dd HH:mm:ss}\t{item.size,13:N0}\t{item.path}");
                 }
                 else
                 {
-                    Console.WriteLine($" {item.name} \t{item.path}\t{item.size}");
+                    Console.WriteLine($" {item.name} \t{item.date:yyyy/MM/dd HH:mm:ss}\t{item.size,13:N0}\t{item.path}");
                 }
                 last = reg.Match(item.name).Value.Replace("-","").ToUpper();
             }
